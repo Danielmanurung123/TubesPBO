268eb82fea3dd2010c1e374527b1573050f249d4
XValensX <91514079+XValensX@users.noreply.github.com>
2024-01-09 00:16:02 +0700

    LogIn.java

--- src/kos/LogIn.java
+++ src/kos/LogIn.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kos;
 
 import java.sql.Connection;
@@ -12,10 +7,6 @@ import java.sql.Statement;
 import javax.swing.JOptionPane;
 import kos.Koneksi;
 
-/**
- *
- * @author PY7
- */
 public class LogIn extends javax.swing.JFrame {
 
     /**
